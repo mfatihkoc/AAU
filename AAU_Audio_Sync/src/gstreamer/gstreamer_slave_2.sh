74f5fe75e374c1238145cdb5757e9ab904461323
beokrt <krt@bang-olufsen.dk>
2023-01-20 08:46:43 +0000

    adding storing gst log for slave2

--- AAU_Audio_Sync/src/gstreamer/gstreamer_slave_2.sh
+++ AAU_Audio_Sync/src/gstreamer/gstreamer_slave_2.sh
@@ -1,4 +1,5 @@
 export GST_DEBUG="*:3"
+export GST_DEBUG_FILE=/${PWD}/slave2_gst.log
 gst-launch-1.0 -v udpsrc address=192.168.50.21 port=12346 ! application/x-rtp,media=audio,clock-rate=48000, channels=1,encoding-name=L16 ! rtpjitterbuffer mode=4  name=jitterBuffer ! rtpL16depay ! audioconvert  ! alsasink alsasink slave-method=1 drift-tolerance=10000 discont-wait=1000 sync=true  sync=true
 
 #Launch Gstreamer with verbose output
