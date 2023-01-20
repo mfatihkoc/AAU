d8da4e3c1192e7002798a61022ea51646f0ac003
beokrt <krt@bang-olufsen.dk>
2023-01-20 08:43:10 +0000

    enabling storing gst logs on disk

--- AAU_Audio_Sync/src/gstreamer/gstreamer_slave_1.sh
+++ AAU_Audio_Sync/src/gstreamer/gstreamer_slave_1.sh
@@ -1,4 +1,5 @@
 export GST_DEBUG="*:3"
+export GST_DEBUG_FILE=/${PWD}/slave1_gst.log
 gst-launch-1.0 -v udpsrc port=12345 ! application/x-rtp,media=audio,clock-rate=48000, channels=1,encoding-name=L16 ! rtpjitterbuffer mode=4  name=jitterBuffer ! rtpL16depay ! audioconvert  ! alsasink slave-method=1 drift-tolerance=10000 discont-wait=1000 sync=true 
 
 #Launch Gstreamer with verbose output
