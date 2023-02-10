b649e7998c6a3f739086664aa5c5a9e7b443186e
mfatihkoc <44057247+mfatihkoc@users.noreply.github.com>
2023-02-10 15:19:26 +0100

    Add files via upload

---
+++ AAU_Audio_Sync/Audio/drift_tol_updated/tol_10/gstreamer_slave_2.sh
@@ -0,0 +1,8 @@
+export GST_DEBUG="*:3"
+export GST_DEBUG_FILE=/${PWD}/slave2_gst.log
+
+
+gst-launch-1.0 -v udpsrc address=192.168.50.20 port=12346 ! application/x-rtp,media=audio,clock-rate=48000, channels=2,encoding-name=L16 ! rtpjitterbuffer mode=0 name=jitterBuffer ! rtpL16depay ! audioconvert ! alsasink slave-method=3 drift-tolerance=10 discont-wait=1000 sync=true 
+
+
+
