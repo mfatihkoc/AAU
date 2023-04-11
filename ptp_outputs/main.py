14e671ef6a56f418699dfc08f951234739aa6e67
mfatihkoc <44057247+mfatihkoc@users.noreply.github.com>
2023-04-11 14:17:29 +0200

    Add files via upload

--- ptp_outputs/main.py
+++ ptp_outputs/main.py
@@ -1,7 +1,9 @@
+
+"""
 import matplotlib.pyplot as plt
 
 # Define the names of the three text files
-file_names = ['phc_1.txt', 'phc_2.txt']
+file_names = ['test1_phc_1stPC.txt', 'test1_phc_2ndPC.txt']
 
 # Initialize lists to store the offset values
 offsets = [[] for _ in range(len(file_names))]
@@ -20,5 +22,31 @@ for i, offset in enumerate(offsets):
     ax.plot(range(len(offset)), offset, label=f'File {i+1}')
 ax.set_xlabel('Sample')
 ax.set_ylabel('PHC Offset')
+plt.savefig('test1_my1_plot.png')
 ax.legend()
+plt.show()
+"""
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read in the phc offset values from the first text file
+with open('test1_phc_1stPC.txt', 'r') as f1:
+    lines1 = f1.readlines()
+    phc_offsets1 = [float(line.split()[4]) for line in lines1]
+
+# Read in the phc offset values from the second text file
+with open('test1_phc_2ndPC.txt', 'r') as f2:
+    lines2 = f2.readlines()
+    phc_offsets2 = [float(line.split()[4]) for line in lines2]
+
+# Plot the histogram for both sets of values
+plt.hist([phc_offsets1, phc_offsets2], bins=50, label=['Client 1', 'Client 2'])
+plt.xlabel('PHC Offset (ns)')
+plt.ylabel('Frequency')
+plt.title('Distribution of PHC Offset Values')
+plt.savefig('test1_my1_plot.png')
+plt.legend()
 plt.show()
